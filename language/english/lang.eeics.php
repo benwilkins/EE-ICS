3436677b62bb9e44f3479e0e412a2d73845770e0
Jesse Bunch <jesse@getbunch.com>
2012-06-18 13:46:43 -0500

    Added boilerplate

---
+++ language/english/lang.eeics.php
@@ -0,0 +1,17 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang = array(	
+	'eeics_module_name' => 
+	'EE-ICS',
+
+	'eeics_module_description' => 
+	'ExpressionEngine module for dynamically generating downloadable ICS files.',
+
+	'module_home' => 'EE-ICS Home',
+	
+// Start inserting custom language keys/values here
+	
+);
+
+/* End of file lang.eeics.php */
+/* Location: /system/expressionengine/third_party/eeics/language/english/lang.eeics.php */
